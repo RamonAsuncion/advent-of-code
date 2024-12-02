7dec0f0fc3969baf562e611e6970384105e455b6
Ramon Asuncion <ramon@b1t.dev>
2024-12-02 07:56:08 -0500

    rewrite.

--- 2024/d1.py
+++ 2024/d1.py
@@ -12,26 +12,14 @@ for ln in ls:
   r.append(n2)
 
 t=0
-# should be using zip
-for i in range(len(l)):
-  # should of sorted the list
-  lm=min(l)
-  rm=min(r)
-  t+=abs(lm-rm)
-  l.remove(lm)
-  r.remove(rm)
-
+l.sort()
+r.sort()
+for x,y in zip(l,r):
+  t+=abs(x-y)
 print(t)
-
-# wouldn't have needed this if it was sorted
-for ln in ls:
-  n1,n2=map(int,ln.split())
-  l.append(n1)
-  r.append(n2)
 
 t=0
 for i in range(len(l)):
-  # i probably could of used collection Counter
   t+=l[i]*r.count(l[i])
 
 print(t)
